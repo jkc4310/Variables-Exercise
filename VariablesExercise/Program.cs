40c093af989535497d61a0fab5d01d1fc8997479
kendall chapman <jkc4310@gmail.com>
2023-06-30 18:07:19 -0400

    Variables Exercise assignment

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            string dogName = "Rex";
+            int age = 5;
+            char initial = 'R';
+            bool isHungry = true;
+            double weight = 14.2;
+            decimal height = 15.3m;
+
+            Console.WriteLine($"Meet my dog {dogName}, but he also goes by {initial}!");
+            Console.WriteLine($"He is {age} years old, {weight} pounds, and stands a whopping {height} inches off the ground.");
+            Console.WriteLine($"It is {isHungry} that he is always hungry!");
+        }  
+
     }
 }
